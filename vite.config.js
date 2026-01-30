6f58bcfecec1730e85883888bcd7aa3ba6495102
Arta Kjato <artakjato@gmail.com>
2026-01-30 14:54:07 +0100

    add node globals

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: ['js-project-happy-thoughts-5d6v.onrender.com']
+  },
   test: {
     projects: [
       {
